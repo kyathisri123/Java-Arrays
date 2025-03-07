9e300699175489e43446ffa2cf3afc8e62402a74
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:26:43 +0530

    Add files via upload

---
+++ ArrayAverage.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class ArrayAverage {
+    public static double averageArray(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("Average: " + averageArray(arr));
+    }
+}
