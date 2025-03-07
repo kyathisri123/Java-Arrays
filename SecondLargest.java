b91f28dcaa8b7593ad2d6d7d4a35668ace05d274
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:33:46 +0530

    Add files via upload

---
+++ SecondLargest.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SecondLargest {
+    public static int findSecondLargest(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length - 2];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("Second Largest: " + findSecondLargest(arr));
+    }
+}
+
