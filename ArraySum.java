f1d25ca8d349bf6e5303d62732d9c364d133a764
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:27:19 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class ArraySum {
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Sum: " + sumArray(arr));
+    }
+}
+
